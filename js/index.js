7924f02038b699bdb4ec269b979bf408fe3e881c
Enrique Mota <evmota@MacBook-Pro-de-Enrique.local>
2020-03-13 00:56:48 -0600

    Modified index.js, working with more than 2 operands

--- js/index.js
+++ js/index.js
@@ -5,77 +5,107 @@ function resetButton(event){
     operador = 0;
     input[0].value = "";
     result.value = "";
+    operadores = [];
+    digitos = [];
+    input[0].focus();
 }
 function equalsButton(event){
     console.log("Clckeaste equals!");
-    switch(operador){
-        case 1:
-            digito2 = Number(input[0].value);
-            result.value = digito + digito2;
-            console.log({digito});
-            console.log({digito2});
-            log.value += "\n" + digito + " " + "+ " + digito2 + " = " + result.value;
-            break;
-        case 2:
-            digito2 = Number(input[0].value);
-            result.value = digito - digito2;
-            console.log({digito});
-            console.log({digito2});
-            log.value += "\n" + digito + " " + "- " + digito2 + " = " + result.value;
-            break;
-        case 3:
-            digito2 = Number(input[0].value);
-            result.value = digito * digito2;
-            console.log({digito});
-            console.log({digito2});
-            log.value += "\n" + digito + " " + "* " + digito2 + " = " + result.value;
-            break;
-        case 4:
-            digito2 = Number(input[0].value);
-            result.value = digito / digito2;
-            console.log({digito});
-            console.log({digito2});
-            log.value += "\n" + digito + " " + "/ " + digito2 + " = " + result.value;
-            break;
+    digito = Number(input[0].value);
+    digitos.push(digito);
+    var stringLog = "\n" + digitos[0] + " " + operadores[0] + " ";
+    var resultado = digitos[0];
+    for(var i = 0; i<operadores.length; i++){
+        switch(operadores[i]){
+            case "+":
+                resultado += digitos[i+1];
+                stringLog += digitos[i+1] + " ";
+                if(i != operadores.length-1){
+                    stringLog += operadores[i+1] + " ";
+                }
+                input[0].focus();
+                break;
+            case "-":
+                resultado -= digitos[i+1];
+                stringLog += digitos[i+1] + " ";
+                if(i != operadores.length-1){
+                    stringLog += operadores[i+1] + " ";
+                }
+                input[0].focus();
+                break;
+            case "*":
+                resultado *= digitos[i+1];
+                stringLog += digitos[i+1] + " ";
+                if(i != operadores.length-1){
+                    stringLog += operadores[i+1] + " ";
+                }
+                input[0].focus();
+                break;
+            case "/":
+                resultado /= digitos[i+1];
+                stringLog += digitos[i+1] + " ";
+                if(i != operadores.length-1){
+                    stringLog += operadores[i+1] + " ";
+                }
+                input[0].focus();
+                break;
+        }
     }
+    stringLog += "= " + resultado;
+    result.value = resultado;
+    log.value += stringLog;
+    
 }
 function sumaButton(event){
     console.log("Clckeaste suma!");
     var numInput = input[0].value;
     digito = Number(numInput);
+    digitos.push(digito);
+    operadores.push("+");
     input[0].value = "";
     operador = 1;
     console.log({digito});
+    input[0].focus();
 }
 function restaButton(event){
     console.log("Clckeaste resta!");
     var numInput = input[0].value;
     digito = Number(numInput);
+    digitos.push(digito);
+    operadores.push("-");
     input[0].value = "";
     operador = 2;
     console.log({digito});
+    input[0].focus();
 }
 function multButton(event){
     console.log("Clckeaste mult!");
     var numInput = input[0].value;
     digito = Number(numInput);
+    digitos.push(digito);
+    operadores.push("*");
     input[0].value = "";
     operador = 3;
     console.log({digito});
+    input[0].focus();
 }
 function divButton(event){
     console.log("Clckeaste div!");
     var numInput = input[0].value;
     digito = Number(numInput);
+    digitos.push(digito);
+    operadores.push("/");
     input[0].value = "";
     operador = 4;
     console.log({digito});
+    input[0].focus();
 }
 
 var digito = 0;
 var digito2 = 0;
 var operador = 0;
-
+var digitos = [];
+var operadores = [];
 
 const input = document.getElementsByClassName("inputNumber");
 const result = document.getElementById("resultValue");
@@ -102,3 +132,5 @@ div.addEventListener('click', divButton);
 console.log("imprimiendo valor...");
 console.log(suma);
 console.log({valor});
+
+input[0].focus();
